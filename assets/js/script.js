81b08b813ba97cc9496a4c29de9c479fb3b18b54
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-06 09:57:17 -0600

    created quiz questions array

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var gameArray = [
+  (one = {
+    question: "Which of the following is not a JS array method?",
+    options: [".pop()", "JSON.parse()", ".slice()", ".concat()"],
+    answer: this.options[2],
+  }),
+  (two = {
+    question:
+      "Which of the following statements about Window Object Properties is false?",
+    options: [
+      "The closed property returns a boolean true if a window is closed.",
+      "The document property returns the frame in which the window runs.",
+      "The outherHeight property returns the height of the browser window, including toolbars/scrollbars.",
+      "The sessionStorage property allows to save key/value pairs in a web browser and stores the data for one session.",
+    ],
+    answer: this.options[2],
+  }),
+  (three = {
+    question:
+      "Cookies are for client-server applications, while local storage is for client applications.",
+    options: ["True", "False"],
+    answer: this.options[1],
+  }),
+  (four = {
+    question: "Which of the following is not an Event Object?",
+    options: ["ClipboardEvent", "MouseEvent", "LifeChangingEvent", "DragEvent"],
+    anser: this.options[3],
+  }),
+];
