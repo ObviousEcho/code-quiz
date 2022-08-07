8d4069b556ca972bf4acb566047508b86f51cbfa
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-07 12:11:20 -0600

    created start game message and button with eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,39 @@ var items = gameArray[index].options;
 
 // ================================================================
 
+beginGame();
+
+function init() {
+appendChildren(list, items);
+questionHeading();
+}
+
+// function creates title and direction elements along with button to begin game
+function beginGame() {
+  var div = document.createElement("div");
+  var h2 = document.createElement("h2");
+  var parag = document.createElement("p");
+  var startBtn = document.createElement("button");
+  h2.textContent ="Test your knowledge of Javascript";
+  parag.textContent = "Click the start button to begin."
+  startBtn.textContent = "Start";
+  div.appendChild(h2);
+  div.appendChild(parag);
+  div.appendChild(startBtn);
+  div.setAttribute("class", "visible");
+  startBtn.setAttribute("class", "start");
+  main.appendChild(div);
+}
+
+// selects div with class start as created in beginGame function
+var startBtnEvent = document.querySelector(".start");
+// changes div class hidden (display: none) to remove content as game progresses
+startBtnEvent.addEventListener("click", function(){
+  var div = document.querySelector(".visible");
+  div.setAttribute("class", "hidden");
+  init();
+})
+
 // function called above in gameArray.options
 // function creates <li><button> with answer options passed in from function call
 function createListItem(text) {
@@ -77,15 +110,12 @@ function questionHeading() {
   main.insertBefore(headingTwo, list);
 }
 
-appendChildren(list, items);
-questionHeading();
-
 // eventListener for button clicks
 // retrieves buttons with class of btn in an array
 var btns = document.getElementsByClassName("btn");
 // stores most recent button clicked in var
 var btnClicked;
-// loop through array of buttons applying eventListener to each one
+// loop through array of buttons applying eventListener to each
 for(i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(event) {
     var element = event.target;
