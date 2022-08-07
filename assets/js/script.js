7137c12aa7c597136dc1ffc6b13a0838fe41dc70
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-06 23:05:22 -0600

    created button element to append li items too, added commets and removed non working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,101 +2,79 @@
 var gameArray = [
   (one = {
     question: "Which of the following is not a JS array method?",
-    options: [createListItem("pop()"), createListItem("JSON.parse()"), createListItem("slice()"), createListItem("concat()")],
+    // the options array calls the createListItem function when each
+    // index is iterated through, passing in a string as an argument
+    options: [
+      createListItem("pop()"),
+      createListItem("JSON.parse()"),
+      createListItem("slice()"),
+      createListItem("concat()"),
+    ],
     answer: "JSON.parse()",
   }),
   (two = {
     question:
-      "Which of the following statements about Window Object Properties is false?",
-    options: [
-      "The closed property returns a boolean true if a window is closed.",
       "The document property returns the frame in which the window runs.",
-      "The outherHeight property returns the height of the browser window, including toolbars/scrollbars.",
-      "The sessionStorage property allows to save key/value pairs in a web browser and stores the data for one session.",
-    ],
-    answer: "The document property returns the frame in which the window runs.",
+    // the options array calls the createListItem function when each
+    // index is iterated through, passing in a string as an argument
+    options: [createListItem("True"), createListItem("False")],
+    answer: "False",
   }),
   (three = {
     question:
       "Cookies are for client-server applications, while local storage is for client applications.",
-    options: ["True", "False"],
+    // the options array calls the createListItem function when each
+    // index is iterated through, passing in a string as an argument
+    options: [createListItem("True"), createListItem("False")],
     answer: "True",
   }),
   (four = {
     question: "Which of the following is not an Event Object?",
-    options: ["ClipboardEvent", "MouseEvent", "LifeChangingEvent", "DragEvent"],
+    // the options array calls the createListItem function when each
+    // index is iterated through, passing in a string as an argument
+    options: [
+      createListItem("ClipboardEvent"),
+      createListItem("MouseEvent"),
+      createListItem("LifeChangingEvent"),
+      createListItem("DragEvent"),
+    ],
     answer: "LifeChangingEvent",
   }),
 ];
 
-var body = document.body;
-// create h1 element
-var headingOne = document.createElement("h1");
-
-// create ordered list
-// var ol = document.createElement("ol");
-// var li = document.createElement("li");
-
-// create p element
-var p = document.createElement("p");
-
-// function support variables
-// var counter = 0;
-// var increment = gameArray[counter];
-
-// headingOne.textContent = "This is a test";
-
-// body.appendChild(mainEl).appendChild(headingOne);
-
-// ================================================================
-// display();
-
-// function displays game question
-// function display() {
-//   gameQuestion();
-//   answerOptions();
-//   if (counter !== gameArray.length) {
-//     counter++;
-//   } else {
-//     counter = 0;
-//   }
-
-// }
-
-// appends game question for use in other functions
-// function gameQuestion() {
-//   headingOne.textContent = increment.question;
-//   body.appendChild(headingOne);
-// }
-
-// appends ol and answer options for use in other functions
-// function answerOptions() {
-//   var arr = gameArray[counter].options;
-//   body.appendChild(ol);
-
-//   for (i = 0; i < arr.length; i++) {
-//     li.textContent = arr[i];
-//     ol.appendChild(li);
-//   }
-// }
-
-
-function createListItem(text) {
-    var li = document.createElement("li");
-    li.textContent = text;
-    return li;
-}
-
-function appendChildren(parent, children) {
-    children.forEach(function (child) {
-        parent.appendChild(child);
-    })
-}
-
+// fetch elements from document
 var main = document.getElementById("main");
 var list = document.getElementById("list");
-var items = gameArray[0].options;
+// var's to help iterate through gameArray
+var index = 0;
+var items = gameArray[index].options;
 
-headingOne.textContent = gameArray[0].question;
-main.insertBefore(headingOne, list);
+// ================================================================
+
+// function called above in gameArray.options
+// function creates <li><button> with answer options passed in from function call
+function createListItem(text) {
+  var button = document.createElement("button");
+  var li = document.createElement("li");
+  li.textContent = text;
+  button.appendChild(li);
+  return button;
+}
+
+// takes two arguments, parent/element and children/array
+// each index within the array is appended to the parent element
+function appendChildren(parent, children) {
+  children.forEach(function (child) {
+    parent.appendChild(child);
+  });
+}
+
+// dynamic creation of quiz question element
+function questionHeading() {
+  var headingTwo = document.createElement("h2");
+  headingTwo.textContent = gameArray[index].question;
+  main.insertBefore(headingTwo, list);
+}
+
 appendChildren(list, items);
+questionHeading();
